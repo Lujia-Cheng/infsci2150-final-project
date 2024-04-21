c6a508c7277bff367b78b9198f1dcbc6430ff68d
Luke Cheng <lukecheng@pitt.edu>
2024-04-21 19:22:37 -0400

    init commit

---
+++ Lab Project 1/mHealth-Project1/app/src/main/java/edu/pitt/lersais/mhealth/LoginActivity.java
@@ -0,0 +1,69 @@
+package edu.pitt.lersais.mhealth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+/**
+ * The LoginActivity is used to handle login authentication, email password authentication.
+ *
+ * @author Haobing Huang and Runhua Xu.
+ */
+
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = "Login_Password_Activity";
+    private static final int REQUEST_SIGNUP = 123;
+
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+
+    // BEGIN
+    private FirebaseAuth mAuth;
+    // END
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // BEGIN
+        mAuth = FirebaseAuth.getInstance();
+        // END
+
+        mEmailEditText = findViewById(R.id.field_email);
+        mPasswordEditText = findViewById(R.id.field_password);
+
+        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
+        findViewById(R.id.link_signup).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.email_sign_in_button) {
+            login();
+        }
+        else if (i == R.id.link_signup) {
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            startActivityForResult(intent, REQUEST_SIGNUP);
+            finish();
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        }
+    }
+
+    private void login() {
+        // TODO: Task 2.3 implement the login code here with validation for user inputs.
+        // Tips: recommended steps
+        // 1) get the email and password from user's input
+        // 2) validate the format of user's input
+        // 3) present a progress dialog (check in the BaseActivity)
+        // 4) call related authentication method provided by Firebase Authentication
+        // 5) close the progress dialog
+
+    }
+
+}
---
+++ Lab Project 1/mHealth-Project1/app/src/main/java/edu/pitt/lersais/mhealth/SignupActivity.java
@@ -0,0 +1,71 @@
+package edu.pitt.lersais.mhealth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+/**
+ * The SignupActivity is used to handle registration, email-password registration.
+ *
+ * @author Haobing Huang and Runhua Xu.
+ */
+
+public class SignupActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = "SignupActivity";
+
+    // BEGIN
+    private FirebaseAuth mAuth;
+    // END
+
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private EditText mConfirmPasswordEditText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // BEGIN
+        mAuth = FirebaseAuth.getInstance();
+        // END
+
+        mEmailEditText = findViewById(R.id.field_email);
+        mPasswordEditText = findViewById(R.id.field_password);
+        mConfirmPasswordEditText = findViewById(R.id.field_password_confirm);
+
+        findViewById(R.id.email_create_account_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.email_create_account_button) {
+            registration();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+
+    private void registration() {
+        // TODO: Task 2.2 implement the registration procedure here.
+        // Tips: recommended steps
+        // 1) get the email and password from user's input
+        // 2) validate the format of user's input
+        // 3) present a progress dialog (check in the BaseActivity)
+        // 4) call related register method provided by Firebase Authentication
+        // 5) close the progress dialog
+
+    }
+}
